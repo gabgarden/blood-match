7ab77162c006e07590114dfbcea5eb07986f4271
Mateus Nogueira <mateuscnogueira2@gmail.com>
2026-06-07 17:31:31 -0300

    feat (infra): adds 2dsphere geospatial index and searches by radius in the donor repository

--- src/main/java/bloodmatch/infra/persistence/schema/DonorSchema.java
+++ src/main/java/bloodmatch/infra/persistence/schema/DonorSchema.java
@@ -3,6 +3,7 @@ package bloodmatch.infra.persistence.schema;
 import bloodmatch.domain.party.Person;
 import bloodmatch.domain.repositories.PersonRepositoryInterface;
 import bloodmatch.domain.roles.person.donor.Donor;
+import bloodmatch.domain.shared.valueObjects.Address;
 import bloodmatch.domain.shared.valueObjects.BloodType;
 import bloodmatch.domain.shared.valueObjects.DomainID;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
@@ -29,6 +32,9 @@ public class DonorSchema {
   private Double weight;
   private LocalDate lastDonationDate;
 
+  @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+  private double[] location;
+
   public DonorSchema(Donor donor) {
     if (donor == null)
       throw new IllegalArgumentException("Donor cannot be null");
@@ -38,6 +44,11 @@ public class DonorSchema {
     this.bloodType = donor.getBloodType().getType();
     this.weight = donor.getWeight();
     this.lastDonationDate = donor.getLastDonationDate();
+
+    Address address = donor.getPerson().getAddress();
+    if (address != null && address.hasCoordinates()) {
+        this.location = new double[]{address.getLongitude(), address.getLatitude()};
+    }
   }
 
   public Donor toDomain(PersonRepositoryInterface personRepository) {
