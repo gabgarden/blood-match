b2ac776df0aff0763891e7a97fcaf93462545482
Mateus Nogueira <mateuscnogueira2@gmail.com>
2026-08-03 23:20:48 -0300

    (feat) implement SMTP email notifications and geographic distance filter

--- src/main/java/bloodmatch/infra/external/notification/ConsoleNotificationService.java
+++ src/main/java/bloodmatch/infra/external/notification/ConsoleNotificationService.java
@@ -4,6 +4,8 @@ import bloodmatch.domain.donationrequest.DonationRequest;
 import bloodmatch.domain.roles.person.donor.Donor;
 import bloodmatch.domain.security.UserAccount;
 import bloodmatch.domain.services.NotificationServiceInterface;
+import bloodmatch.domain.shared.valueObjects.Address;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,12 +18,16 @@ public class ConsoleNotificationService implements NotificationServiceInterface 
         String bloodCenterName = request.getBloodCenter().getOrganization().getName();
         String bloodType = donor.getBloodType().getType();
 
+        Address addr1 = donor.getPerson().getAddress();
+        Address addr2 = request.getBloodCenter().getOrganization().getAddress();
+        double distance = Math.round(addr1.distanceTo(addr2) * 10.0) / 10.0;
+
         System.out.println("=====================================================");
         System.out.println("[NOTIFICAÇÃO ENVIADA - SIMULAÇÃO PUSH/EMAIL]");
         System.out.println("Para: " + donorName + " (" + donorEmail + ")");
         System.out.println("Assunto: Precisamos de doadores de sangue " + bloodType + "!");
-        System.out.println("Mensagem: Olá " + donorName + ", o " + bloodCenterName + 
-                           " está precisando de doações. Acesse o sistema para agendar!");
+        System.out.println("Mensagem: Olá " + donorName + ", o " + bloodCenterName + " está precisando de doações. Acesse o sistema para agendar!");
+        System.out.println("Distância até o local: " + distance + "km.");
         System.out.println("=====================================================");
     }
 }
